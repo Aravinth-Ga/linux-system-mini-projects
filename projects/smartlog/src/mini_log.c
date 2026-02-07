517631d72e08775342f8eccc5389eb1fc990eae7
Aravinth Ganesan <aravintheng@gmail.com>
2026-02-07 19:11:40 +0100

    mini_log: fix stat error handling and distinguish ENOENT from real failures

--- projects/smartlog/src/mini_log.c
+++ projects/smartlog/src/mini_log.c
@@ -21,6 +21,7 @@
 
 */
 
+#define _POSIX_C_SOURCE 199309L
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -31,13 +32,13 @@
 #include <string.h>
 #include <time.h>
 
-#define ERROR_MSG   0
-#define LOG_MSG     1
+#define MESSAGE_LEN_MAX   200
+
 
 uint64_t time_stamp(void)
 {
     struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+    clock_gettime(CLOCK_REALTIME, &ts);
 
     return ((uint64_t) (ts.tv_sec * 1000000000ull) + (uint64_t)ts.tv_nsec);
 }
@@ -75,11 +76,9 @@ static int write_all(int file_descriptor, const void* msg_buff, size_t msg_lengt
     return 0;
 }
 
-static int write_usage(void)
+static int write_usage(const char* error_msg)
 {
-    char* info = "Usage : ./mini_log <file_path> \"<message>\" [--durable]\n";
-
-    return write_all(STDERR_FILENO, info, strlen(info));
+    return write_all(STDERR_FILENO, error_msg, strlen(error_msg));
 }
 
 int main(int argc, char* argv[])
@@ -95,24 +94,59 @@ int main(int argc, char* argv[])
         }
         else
         {
-            return write_usage();
-        }
+            return write_usage("Usage : ./mini_log <file_path> \"<message>\" [--durable]\n");
+        }  
     }
     else if(argc != 3)
     {
-        return write_usage();
+        return write_usage("Usage : ./mini_log <file_path> \"<message>\" [--durable]\n");
     }
 
     const char* file_path = argv[1];
     const char* msg = argv[2];
-   
+    size_t msg_len = strlen(msg);
 
-    // 2. Open the file
+    if(msg[0] == '\0')
+        return write_usage("Log message is Empty.\n");
+   
+    // 2. Permission check
+    struct stat fstat_old;
+    int file1_exist = stat(file_path, &fstat_old); 
+    int stat1_errno = errno;
+
+    if(file1_exist == 0) 
+    {
+        if(S_ISDIR(fstat_old.st_mode) != 0)
+        {
+            fprintf(stderr, "Error : %s is a directory.\n",file_path);
+            return 1;
+        }
+        else
+        {
+            printf("[LOG FILE PERMISSION CHECK]\n");            
+            printf("The file %s exist.\n", file_path);
+            printf("Owner UID : %u\n",fstat_old.st_uid);
+            printf("Group ID : %u\n", fstat_old.st_gid);
+            printf("File Permission : %04o\n", fstat_old.st_mode & 0777);        
+        }             
+    }
+    else 
+    {
+        if(stat1_errno != ENOENT)
+        {   
+            perror("stat");
+            return 1;
+        }
+
+    }
+
+    // 3. Open the file
 
     // filemode
-    mode_t mode = S_IRUSR | S_IWUSR | S_IROTH;
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP;
 
-    // open flag
+    // open flag 
+    // O_APPEND is added to update the file EOF automatically.
     int flag = O_WRONLY | O_CREAT | O_APPEND;
 
     int fd = open(file_path, flag, mode);
@@ -123,16 +157,44 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // 3. Permission check
+    struct stat fstat_new;
+    int file2_exist = stat(file_path, &fstat_new);
+    int stat2_errno = errno;
     
-
+    
+    if(file1_exist !=0 && stat1_errno == ENOENT)
+    {
+        if(file2_exist == 0)
+        {
+            printf("[LOG FILE PERMISSION CHECK]\n");            
+            printf("The file %s does not exist and it is created now.\n",file_path);
+            printf("Owner UID : %u\n",fstat_new.st_uid);
+            printf("Group ID : %u\n", fstat_new.st_gid);
+            printf("File Permission : %04o\n", fstat_new.st_mode & 0777);        
+        }
+        else
+        {
+            perror("stat");
+            close(fd);
+            return 1;
+        }
+    }
     // 4. Write the data into the log file
 
+    char buff[MESSAGE_LEN_MAX + 1];
+    if(msg_len > MESSAGE_LEN_MAX)
+    {
+        memcpy(buff, msg, (MESSAGE_LEN_MAX - 3));
+        memcpy(buff + (MESSAGE_LEN_MAX - 3), "...", 3);
+        buff[MESSAGE_LEN_MAX] = '\0';
+        msg = buff;
+    }
+
     uint64_t time_ns = time_stamp();
-    uint8_t pid = getgid();   
+    pid_t pid = getpid();   
     char log_buffer[1024];
     
-    int log_len = snprintf(log_buffer, sizeof(log_buffer), "[%lu ns] [pid = %d] [%s]\n", time_ns, pid, msg);
+    int log_len = snprintf(log_buffer, sizeof(log_buffer), "[%llu ns] [PID = %ld] [MESSAGE = %s]\n", (unsigned long long)time_ns, (long)pid, msg);
 
     if(log_len < 0 || (log_len >= (int)sizeof(log_buffer)))
     {
